32c49f3e86a9f4d5ed8af30f4fdc6039f525a7d0
ShivamShrirao <shivamshrirao@gmail.com>
2021-01-20 18:48:59 +0530

    more options and flipped

--- live_webcam.py
+++ live_webcam.py
@@ -1,23 +1,27 @@
 import time
 from tqdm import tqdm
 import torch
-import pyfakewebcam			# SET ME
 import numpy as np
 import imageio
 from skimage.transform import resize
 from argparse import ArgumentParser
+from threading import Thread
 import cv2
 
 from demo import load_checkpoints
 from animate import normalize_kp
 
-cpu=False
 
 parser = ArgumentParser()
 parser.add_argument("--source_image", default='../../Downloads/first_order/02.png', help="path to source image")
-parser.add_argument("--fake_cam", default='/dev/video0', help="webcam stream path")
+parser.add_argument("--source_cam", default=0, help="source webcam integer")
+parser.add_argument("--fake_cam_path", default=None, help="fake webcam path")
+parser.add_argument("--fps", default=30, help="output schedule_frame fps")
+parser.add_argument("--checkpoint_path", default='../../Downloads/vox-cpk.pth.tar', help="checkpoint path")
 opt = parser.parse_args()
 
+cpu=False
+fps = int(opt.fps)
 source_image = imageio.imread(opt.source_image)
 source_image = (resize(source_image, (256, 256))[..., :3]).astype(np.float32)
 
@@ -38,9 +42,10 @@ def make_animate_live(driving_video, generator, kp_detector, relative=True, adap
 
 generator, kp_detector = load_checkpoints(config_path='config/vox-256.yaml', checkpoint_path='../../Downloads/vox-cpk.pth.tar')
 
-
-camera = pyfakewebcam.FakeWebcam(opt.fake_cam, 256, 256)	# SET ME
-cap = cv2.VideoCapture(1)				# SET ME
+if opt.fake_cam_path:
+	import pyfakewebcam
+	fake_camera = pyfakewebcam.FakeWebcam(opt.fake_cam_path, 256, 256)
+cap = cv2.VideoCapture(int(opt.source_cam))
 
 ret, frame = cap.read()
 slcs = (slice(120, -140, None), slice(455, -455, None))
@@ -57,20 +62,29 @@ with torch.no_grad():
 	kp_driving_initial = kp_detector(driving[:, :, 0])
 	kp_source = kp_detector(source)
 
+out = source_image
+def thread_schedule_fake():
+	while True:
+		fake_camera.schedule_frame(out)
+		time.sleep(1/fps)
+
+if opt.fake_cam_path:
+	t = Thread(target=thread_schedule_fake)
+	t.setDaemon(True)
+	t.start()
+
 while True:
 	ret, frame = cap.read()
 	frame = frame[slcs]
 	cv2.imshow('frame', frame)
 	frame = cv2.resize(frame, (256, 256))
 	img = frame.astype(np.float32)/255
-
 	pred = make_animate_live([img], generator, kp_detector)
 
-	out = (pred*255).astype(np.uint8)
+	out = cv2.flip((pred*255).astype(np.uint8), 1)
 	cv2.imshow('out', cv2.cvtColor(out, cv2.COLOR_RGB2BGR))
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
-	camera.schedule_frame(out)					# SET ME
 
 cap.release()
 cv2.destroyAllWindows()
